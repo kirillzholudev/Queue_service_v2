630264bb45ce7edc3a4b73c25ce021c2539af851
kirillzholudev <kirillzholudev24@gmail.com>
2022-10-31 15:35:32 +0100

    refactor models + admins files

--- general/admin.py
+++ general/admin.py
@@ -4,7 +4,10 @@ from general.models import Company, Specialization, Worker
 
 admin.site.register(Company)
 admin.site.register(Specialization)
-admin.site.register(Worker)
+#admin.site.register(Worker)
 
 
 
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'start_shift', 'finish_shift', 'current_time']
--- general/models.py
+++ general/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
+import time
 
 
 class Specialization(models.Model):
@@ -21,8 +23,18 @@ class Company(models.Model):
 class Worker(models.Model):
     name = models.CharField(max_length=50, null=True)
     specialization = models.ManyToManyField(Specialization)
-    start_shift = models.TimeField(auto_now_add=False)
-    finish_shift = models.TimeField(auto_now_add=False)
+    start_shift = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    finish_shift = models.DateTimeField(auto_now_add=False, null=True, blank=True)
 
     def __str__(self):
         return f'{self.name} {self.start_shift} {self.finish_shift}'
+
+    @property
+    def current_time(self):
+        return f"{self.__current_time} min"
+
+    @property
+    def __current_time(self):
+        if self.finish_shift:
+            return round((self.finish_shift.timestamp() - self.start_shift.timestamp())/60)
+        return round((time.time() - self.start_shift.timestamp())/60)
